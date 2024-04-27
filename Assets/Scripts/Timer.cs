92698266cbe0f43987032745ea2be4e5b4e90924
SeaLanternn <haidang9079@gmail.com>
2024-04-27 15:53:50 +1200

    change timer to fit more into the game

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UIElements.Experimental;
 
 // NOTE: 	You can create a new timer in script, use the static CreateTimer method.
-//			This timer is meant to use for counting seconds, although it technically works for smaller time value if not paused
+//			This timer is meant to use for counting milliseconds, although it technically works for smaller time value if not paused
 //			The timeRemaining variable will be updated each second(unless timeRemaining is less than 1, then it will update after the remaining amount of time)
 //			If paused while the time is not rounded, the remaining time will be rounded up (EX: 5.672 --> 6; 0.2 --> 1)
 
@@ -36,14 +36,14 @@ public class Timer : MonoBehaviour
 			duration = Mathf.Clamp(duration,0f, 86400f);
 			timeRemaining = duration;
 			if (duration < 1) {Invoke ( "_tick", duration);}
-            else Invoke ("_tick", 1f );
+            else Invoke ("_tick", 0.1f );
         }
     }
 
     private void _tick() {
 		if (isPaused == true) {return;}
-        timeRemaining--;
-        if (timeRemaining > 1) {Invoke ( "_tick", 1f );}
+        timeRemaining-= 0.1f;
+        if (timeRemaining > 0.1) {Invoke ( "_tick", 0.1f );}
 		else if (timeRemaining > 0) {Invoke( "_tick", timeRemaining);}
 		else
 		{
@@ -57,7 +57,7 @@ public class Timer : MonoBehaviour
 	private void SetPause(bool value)
 	{
 		isCountingDown = !value;
-		if(value == false && timeRemaining > 0) {Invoke ( "_tick", 1f);}
+		if(value == false && timeRemaining > 0) {Invoke ( "_tick", 0.1f);}
 	}
 
 	public static Timer CreateTimer(GameObject ObjectToAttach, float Duration, bool looptimer, bool DestroyOnEnd)
